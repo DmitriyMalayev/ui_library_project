45f091c56ccef6a31c65a2e788e1b7316be2d484
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-06 23:11:00 -0400

    added css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,23 @@ module.exports = {
           presets: ['@babel/preset-env']
         }
       }
-    }]
+    },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      
+    }
+    ]
   }
 };
+/*
+This is where we configure the rules for our modules.
+Order of loaders is important, they run from right to left
+css-loader 
+  Collects the CSS from the CSS Files when we import them
+style-loader
+  Adds the CSS to the HTML document
+npm serve
+  Starts our web server
+
+  */
